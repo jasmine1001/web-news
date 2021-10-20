2d2aa9cbd76a865f7a9fb1fea234e916f04d8e21
jasmine sawra <jasminsawra@gmail.com>
2021-10-20 17:04:56 +0530

    changes in html

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("hey");
+// ab8d9b00779642a3b682931b7cf1622d
+let country = 'in';
+let apiKey = 'ab8d9b00779642a3b682931b7cf1622d'
+let news = document.getElementById('news');
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`, true);
+
+
+xhr.onload = function () {
+  if (this.status === 200) {
+    let json = JSON.parse(this.responseText);
+    let articles = json.articles;
+    console.log(articles);
+
+    let newsHtml = "";
+    articles.forEach(function (element,index) {
+// console.log(element,index)
+      let Todaynews = `
+<p>
+<button id="news" class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1">${element["title"]}</button>
+
+</p>
+<div class="row">
+<div class="col">
+  <div class="collapse collapse show" id="multiCollapseExample1">
+    <div class="card card-body">
+      ${element["content"]} .<a href="${element['url']}" target="_blank"> Read more here</a>
+    </div>
+  </div>
+</div>
+  
+</div>`
+      newsHtml += Todaynews;
+    });
+    news.innerHTML = newsHtml;
+  }
+  else {
+    console.log("some error occured");
+  }
+}
+
+xhr.send()
+
+
+
